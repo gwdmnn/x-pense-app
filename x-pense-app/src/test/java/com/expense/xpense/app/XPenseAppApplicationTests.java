cfc73cdb9769be43f89ed93c437d172ff97b5e77
Guilherme <guilherme.weidmann@gmail.com>
2023-01-14 11:32:34 -0300

    First commit

---
+++ x-pense-app/src/test/java/com/expense/xpense/app/XPenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.xpense.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPenseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
